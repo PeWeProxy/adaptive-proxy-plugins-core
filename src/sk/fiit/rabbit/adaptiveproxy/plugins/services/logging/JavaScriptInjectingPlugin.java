4bc37b84a04e290c2c3cde71432005d4dd593b48
Tomas Kramar <kramar.tomas@gmail.com>
2009-11-10 01:01:09 +0100

    JavascriptInjectingPlugin
    Vseobecny plugin ktory sa nahadzuje len vytvorenim konfiguraku
    kde sa uvedie cesta k skriptu a bypassy.
    
    Momentalne sluzi ako KeywordsDisplay a ActivityUpdater

--- src/sk/fiit/rabbit/adaptiveproxy/plugins/services/logging/JavaScriptInjectingPlugin.java
+++ src/sk/fiit/rabbit/adaptiveproxy/plugins/services/logging/JavaScriptInjectingPlugin.java
@@ -2,27 +2,44 @@ package sk.fiit.rabbit.adaptiveproxy.plugins.services.logging;
 
 import org.apache.log4j.Logger;
 
+import sk.fiit.rabbit.adaptiveproxy.plugins.PluginProperties;
 import sk.fiit.rabbit.adaptiveproxy.plugins.helpers.ResponseProcessingPluginAdapter;
 import sk.fiit.rabbit.adaptiveproxy.plugins.messages.ModifiableHttpResponse;
 import sk.fiit.rabbit.adaptiveproxy.plugins.services.ServiceUnavailableException;
 import sk.fiit.rabbit.adaptiveproxy.plugins.services.injector.JavaScript;
 import sk.fiit.rabbit.adaptiveproxy.plugins.services.injector.JavaScriptInjectorService;
 
-public class KeywordsDisplayPlugin extends ResponseProcessingPluginAdapter {
+public class JavaScriptInjectingPlugin extends ResponseProcessingPluginAdapter {
+
+	Logger logger = Logger.getLogger(JavaScriptInjectingPlugin.class);
 	
-	Logger logger = Logger.getLogger(KeywordsDisplayPlugin.class);
-	
+	private String scriptUrl;
+	private String bypassPattern;
+	private String bypassTo;
+
+	public JavaScriptInjectingPlugin() {
+		super();
+	}
+
 	@Override
 	public ResponseProcessingActions processResponse(ModifiableHttpResponse response) {
 		try {
 			JavaScriptInjectorService injector = response.getServiceHandle().getService(JavaScriptInjectorService.class);
-			injector.registerJavascript(new JavaScript("http://127.0.0.1:3000/javascripts/application.js", "keywords/load", "http://127.0.0.1:3000/keywords/load"));
+			injector.registerJavascript(new JavaScript(scriptUrl, bypassPattern, bypassTo));
 		} catch (ServiceUnavailableException e) {
 			logger.warn("JavaScriptInjector unavailable");
 		}
-
+	
 		return ResponseProcessingActions.PROCEED;
 	}
 	
-	
+	@Override
+	public boolean setup(PluginProperties props) {
+		scriptUrl = props.getProperty("scriptUrl");
+		bypassPattern = props.getProperty("bypassPattern");
+		bypassTo = props.getProperty("bypassTo");
+		
+		return true;
+	}
+
 }
