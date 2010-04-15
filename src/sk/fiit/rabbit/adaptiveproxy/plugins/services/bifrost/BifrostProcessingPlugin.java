ccd6210e16856e7d4042cd38fffcc58f4e0d60d2
Tomas Kramar <kramar.tomas@gmail.com>
2010-04-15 15:00:08 +0200

    Add bifrost integration plugin

---
+++ src/sk/fiit/rabbit/adaptiveproxy/plugins/services/bifrost/BifrostProcessingPlugin.java
@@ -0,0 +1,80 @@
+package sk.fiit.rabbit.adaptiveproxy.plugins.services.bifrost;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import sk.fiit.bifrost.dunco.Document;
+import sk.fiit.bifrost.dunco.FetchException;
+import sk.fiit.bifrost.dunco.WebSearch;
+import sk.fiit.bifrost.dunco.google.GoogleWebSearch;
+import sk.fiit.bifrost.recommendations.Context;
+import sk.fiit.bifrost.recommendations.KeywordsCompletionStrategy;
+import sk.fiit.bifrost.recommendations.QueryRedefinitionsRecommender;
+import sk.fiit.bifrost.recommendations.RecommendationStrategy;
+import sk.fiit.rabbit.adaptiveproxy.plugins.messages.HttpMessageFactory;
+import sk.fiit.rabbit.adaptiveproxy.plugins.messages.HttpResponse;
+import sk.fiit.rabbit.adaptiveproxy.plugins.messages.ModifiableHttpRequest;
+import sk.fiit.rabbit.adaptiveproxy.plugins.messages.ModifiableHttpResponse;
+import sk.fiit.rabbit.adaptiveproxy.plugins.services.ServiceUnavailableException;
+import sk.fiit.rabbit.adaptiveproxy.plugins.services.content.ModifiableStringService;
+import sk.fiit.rabbit.adaptiveproxy.plugins.services.injector.JavaScriptInjectingProcessingPlugin;
+
+public class BifrostProcessingPlugin extends JavaScriptInjectingProcessingPlugin {
+	
+	private WebSearch searcher = new GoogleWebSearch();
+	private RecommendationStrategy coKeywordsStrategy = new KeywordsCompletionStrategy();
+	private QueryRedefinitionsRecommender queryRedefinitionStrategy = new QueryRedefinitionsRecommender();
+	
+	private static final int MAX_QUERIES = 2;
+	private static final int MAX_DOCUMENTS_FROM_QUERY = 2;
+	
+	@Override
+	public HttpResponse getResponse(ModifiableHttpRequest proxyResponse, HttpMessageFactory messageFactory) {
+		String query = extractQueryFromURI(proxyResponse.getClientRequestHeaders().getRequestURI());
+		
+		Context context = new Context();
+		context.setLastQuery(query);
+		
+		Collection<String> queries = coKeywordsStrategy.recommendQueries("TODO", context); //TODO
+		Collection<Document> resultDocuments = new LinkedList<Document>();
+		
+		ModifiableHttpResponse response = messageFactory.constructHttpResponse(true);
+		
+		try {
+			int queryCount = 0;
+			for(String q : queries) {
+				if(++queryCount > MAX_QUERIES) break;
+				try {
+					int documentCount = 0;
+					for(Document doc : searcher.search(q)) {
+						if(++documentCount > MAX_DOCUMENTS_FROM_QUERY) break;
+						resultDocuments.add(doc);
+					}
+				} catch (FetchException e) {
+					logger.warn("Could not fetch search results for query: " + q, e);
+				}
+			}
+
+			String resultHtml = GoogleResultsFormatter.format(resultDocuments);
+			
+			response.getServiceHandle().getService(ModifiableStringService.class).setContent(resultHtml);
+			response.getProxyResponseHeaders().addHeader("Content-Type", "text/plain; charset=ISO-8859-1");
+		} catch (ServiceUnavailableException e) {
+			logger.error("ModifiableStringService is unavailable, cannot generate new response");
+		}
+		
+		return response;
+	}
+	
+	private String extractQueryFromURI(String uri) {
+		Pattern pattern = Pattern.compile(".*[?&]q=(.*?)(:?$|&.*)");
+		Matcher matcher = pattern.matcher(uri);
+		if(matcher.matches()) {
+			return matcher.group(1).replace("%2B", " ").replace("+", " "); 
+		} else {
+			return null;
+		}
+	}
+}
---
+++ src/sk/fiit/rabbit/adaptiveproxy/plugins/services/bifrost/GoogleResultsFormatter.java
@@ -0,0 +1,30 @@
+package sk.fiit.rabbit.adaptiveproxy.plugins.services.bifrost;
+
+import java.util.Collection;
+
+import sk.fiit.bifrost.dunco.Document;
+
+public class GoogleResultsFormatter {
+    
+	public static String format(Collection<Document> documents) {
+		String html = "";
+		
+		for(Document doc : documents) {
+			html+= 
+				"<li class='g w0'>" +
+					"<h3 class='r'>" +
+						"<a class='l' href='" + doc.getUrl() +  "'>" + doc.getTitle() + "</a>" +
+					"</h3>" +
+					"<div class='s'>" + 
+						doc.getContent() + 
+						"<br/>" +
+						"<cite>" + doc.getUrl() + "</cite>" +
+					"</div>" +
+				"</li>";
+		}
+		
+		html = "<div style='border: 1px solid green;'>" + html + "</div>";
+		
+		return html;
+	}
+}
