8436260583cd93a2a26411ed5e67088ed4b5f90d
Michal Holub <miso.holub@gmail.com>
2010-04-25 19:30:51 +0200

    Add namespace to feedback function call

--- src/sk/fiit/rabbit/adaptiveproxy/plugins/services/webimp/feedback/Feedback.java
+++ src/sk/fiit/rabbit/adaptiveproxy/plugins/services/webimp/feedback/Feedback.java
@@ -2,8 +2,8 @@ package sk.fiit.rabbit.adaptiveproxy.plugins.services.webimp.feedback;
 
 public class Feedback {
 	public static String getCode(final String directory) {
-		String code = "<img style=\"cursor:pointer\" src=\"" + directory + "/like.png\" onClick=\"wiSendFeedback(1);\" alt=\"Stránka ma zaujala\" width=\"30\" height=\"30\"/>";
-		code += "<img style=\"cursor:pointer\" src=\"" + directory + "/dislike.png\" onClick=\"wiSendFeedback(-1);\" alt=\"Stránka ma nezaujala\" width=\"30\" height=\"30\"/>";
+		String code = "<img style=\"cursor:pointer\" src=\"" + directory + "/like.png\" onClick=\"wiFeedback.wiSendFeedback(1);\" alt=\"Stránka ma zaujala\" width=\"30\" height=\"30\"/>";
+		code += "<img style=\"cursor:pointer\" src=\"" + directory + "/dislike.png\" onClick=\"wiFeedback.wiSendFeedback(-1);\" alt=\"Stránka ma nezaujala\" width=\"30\" height=\"30\"/>";
 		/*
 		String code = "<a href=\"www.fiit.sk\">";
 		code += "<img src='" + directory + "/star-bad.png' alt='Úplne nezaujala' width='15' height='15'/>";
