92f1c4a1d575e576126a59d7a5af36a5429e2de5
Jan Hudek <hudek.jan@gmail.com>
2010-10-20 20:03:59 +0200

    Replace update query with insert
    Insert whole new record instead of updating

--- src/sk/fiit/rabbit/adaptiveproxy/plugins/services/user/UserRecognitionPlugin.java
+++ src/sk/fiit/rabbit/adaptiveproxy/plugins/services/user/UserRecognitionPlugin.java
@@ -2,7 +2,9 @@ package sk.fiit.rabbit.adaptiveproxy.plugins.services.user;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,13 +19,14 @@ import sk.fiit.rabbit.adaptiveproxy.plugins.messages.HttpResponse;
 import sk.fiit.rabbit.adaptiveproxy.plugins.messages.ModifiableHttpRequest;
 import sk.fiit.rabbit.adaptiveproxy.plugins.messages.ModifiableHttpResponse;
 import sk.fiit.rabbit.adaptiveproxy.plugins.services.ServiceUnavailableException;
+import sk.fiit.rabbit.adaptiveproxy.plugins.services.common.Checksum;
 import sk.fiit.rabbit.adaptiveproxy.plugins.services.common.SqlUtils;
 import sk.fiit.rabbit.adaptiveproxy.plugins.services.content.ModifiableStringService;
 import sk.fiit.rabbit.adaptiveproxy.plugins.services.content.StringContentService;
 import sk.fiit.rabbit.adaptiveproxy.plugins.services.database.DatabaseConnectionProviderService;
 import sk.fiit.rabbit.adaptiveproxy.plugins.services.injector.HtmlInjectorService;
-import sk.fiit.rabbit.adaptiveproxy.plugins.services.injector.JavaScriptInjectingProcessingPlugin;
 import sk.fiit.rabbit.adaptiveproxy.plugins.services.injector.HtmlInjectorService.HtmlPosition;
+import sk.fiit.rabbit.adaptiveproxy.plugins.services.injector.JavaScriptInjectingProcessingPlugin;
 
 public class UserRecognitionPlugin extends RequestAndResponseProcessingPluginAdapter {
 	
@@ -42,6 +45,7 @@ public class UserRecognitionPlugin extends RequestAndResponseProcessingPluginAda
 	{
 		Map<String, String> postData;
 		
+		
 		if(request.getClientRequestHeaders().getRequestURI().contains(bypassPattern))
 		{
 			try {
@@ -51,11 +55,14 @@ public class UserRecognitionPlugin extends RequestAndResponseProcessingPluginAda
 	
 				Connection con = null;
 				
-				if (postData.containsKey("__peweproxy_uid") && postData.containsKey("__peweproxy_pid") && postData.containsKey("__peweproxy_timestamp"))	
+				if (postData.containsKey("__peweproxy_uid") && postData.containsKey("_ap_checksum") && postData.containsKey("__ap_url"))	
 				{
 					try {
 						con = request.getServiceHandle().getService(DatabaseConnectionProviderService.class).getDatabaseConnection();
-						updateDatabaseLog(con, postData.get("__peweproxy_uid"), postData.get("__peweproxy_pid"), postData.get("__peweproxy_timestamp"));
+						
+						//createDatabaseLog(con, postData.get("__peweproxy_uid"), postData.get("_ap_checksum"), postData.get("__ap_url"), request.getClientSocketAddress().getAddress().toString());
+						createDatabaseLog(con, postData.get("__peweproxy_uid"), postData.get("_ap_checksum"), postData.get("__ap_url"), "ip");
+						
 					} finally {
 						SqlUtils.close(con);
 					}
@@ -73,17 +80,55 @@ public class UserRecognitionPlugin extends RequestAndResponseProcessingPluginAda
 		
 	}
 	
-	private boolean updateDatabaseLog(Connection connection, String uid, String pid, String timestamp)
+	private boolean createDatabaseLog(Connection connection, String uid, String checksum, String url, String ip)
 	{		
 		PreparedStatement stmt = null;
+		PreparedStatement stmt_2 = null;
+		
+		String pid = "";
+		java.util.Date today = new java.util.Date();
+		String timestamp = new Timestamp(today.getTime()).toString();
+		String formatedTimeStamp = timestamp.substring(0, timestamp.indexOf("."));
+		
+		url = url.replace("%2F", "/");
+		url = url.replace("%3A", ":");
+		url = url.replace("%3F", "?");
+		url = url.replace("%26", "&");
+		url = url.replace("%3D", "=");
+		
 		
 		try {
-			stmt = connection.prepareStatement("UPDATE access_logs SET userid=? WHERE id=? AND timestamp=?");
-			stmt.setString(1, uid);
-			stmt.setString(2, pid);
-			stmt.setString(3, timestamp);
+			stmt = connection.prepareStatement("SELECT * FROM pages WHERE url=? AND checksum =? ORDER BY id DESC LIMIT 1;");
+			stmt.setString(1, url);
+			stmt.setString(2, checksum);
+			
 			
 			stmt.execute();
+			ResultSet rs = stmt.getResultSet();
+			
+			while (rs.next()) {
+				pid = rs.getString(1);
+			}
+			
+			try {
+				stmt_2 = connection.prepareStatement("INSERT INTO `proxy`.`access_logs` (`id`, `userid`, `timestamp`, `time_on_page`, `page_id`, `scroll_count`, `copy_count`, `referer`, `ip`) VALUES (NULL, ?, ?, NULL, ?, NULL, NULL, ?, ?);");
+
+
+				stmt_2.setString(1, uid);
+				stmt_2.setString(3, pid);
+				stmt_2.setString(4, url);
+				stmt_2.setString(5, Checksum.md5(ip.substring(1)));
+								
+				stmt_2.setString(2, formatedTimeStamp);
+				
+				
+				stmt_2.execute();
+			} catch (SQLException e) {
+				logger.error("Could not set users uid for access log", e);
+			} finally {
+				SqlUtils.close(stmt_2);
+			}
+			
 		} catch (SQLException e) {
 			logger.error("Could not set users uid for access log", e);
 		} finally {
@@ -91,6 +136,27 @@ public class UserRecognitionPlugin extends RequestAndResponseProcessingPluginAda
 		}
 		
 		return true;
+	}
+	
+	
+	
+	private void log(Connection connection, String userId, Long pageId, String referer, String ipAddress) {
+		PreparedStatement stmt = null;
+
+		try {
+			stmt = connection.prepareStatement("INSERT INTO access_logs(userid, timestamp, page_id, referer, ip) VALUES(?, ?, ?, ?, ?)");
+			stmt.setString(1, userId);
+			stmt.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
+			stmt.setLong(3, pageId);
+			stmt.setString(4, referer);
+			stmt.setString(5, ipAddress);
+			
+			stmt.execute();
+		} catch (SQLException e) {
+			logger.error("Could not save access log", e);
+		} finally {
+			SqlUtils.close(stmt);
+		}
 	}
 	
 	
