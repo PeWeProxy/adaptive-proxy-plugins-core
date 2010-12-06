4fd0fd0eb229a716e363581ee82f28d06a73d5a3
Pavol Sokol <p.sokol@gmail.com>
2010-12-06 22:38:19 +0100

    Save and load pages_terms and terms

--- src/sk/fiit/rabbit/adaptiveproxy/plugins/services/page/CachingPageInformationProviderServiceModule.java
+++ src/sk/fiit/rabbit/adaptiveproxy/plugins/services/page/CachingPageInformationProviderServiceModule.java
@@ -14,10 +14,16 @@ import java.nio.Buffer;
 import java.nio.charset.Charset;
 import java.nio.charset.UnsupportedCharsetException;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
@@ -25,6 +31,8 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 
 import rabbit.util.CharsetUtils;
 import sk.fiit.peweproxy.headers.ReadableHeader;
@@ -86,8 +94,11 @@ public class CachingPageInformationProviderServiceModule implements ResponseServ
 			
 			return pi;
 		}
+
+		
 		
 		private void extractPageInformation(PageInformation pi) {
+			JSONArray jsonArray = null;
 			try {
 				connection = connectionService.getDatabaseConnection();
 				
@@ -98,13 +109,18 @@ public class CachingPageInformationProviderServiceModule implements ResponseServ
 				if(pi.id == null && clearText != null) {
 					pi.contentLength = clearText.length();
 					try {
-						pi.keywords = extractKeywords(content, charset);
+						jsonArray = extractKeywords(content, charset);
+					} catch (MalformedURLException e) {
+						logger.error("Malformed URL for Metall/meta keywords extraction service:", e);
 					} catch (IOException e) {
 						// TODO: some error with response 500, when sending img url
-						logger.debug("Metall meta keywords extraction client FAILED:"+e.getMessage());
+						logger.debug("Metall meta keywords extraction client FAILED:"+e.getMessage()+" for URI:"+requestURI);
 					}
-					
-					savePageInformation(pi);
+					if(jsonArray == null) {
+						return;
+					}
+					pi.setPageTermsList(Json2PagesTerms(jsonArray));
+					save(pi);
 				}
 			} finally {
 				SqlUtils.close(connection);
@@ -142,6 +158,11 @@ public class CachingPageInformationProviderServiceModule implements ResponseServ
 					pi.contentLength = rs.getInt(2);
 					pi.keywords = rs.getString(3);
 				}
+
+				if(pi.getId() != null) {
+					loadPagesTermsFromCache(pi);
+				}
+				
 			} catch(SQLException e) {
 				logger.error("Could not load pageId from cache", e);
 			} finally {
@@ -150,12 +171,70 @@ public class CachingPageInformationProviderServiceModule implements ResponseServ
 			}
 		}
 		
-			public String extractKeywords(String content, String chrset) throws MalformedURLException, IOException {
+		private void loadPagesTermsFromCache(PageInformation pi) {
+
+			if(connection == null) {
+				return;
+			}
+			
+			PreparedStatement stmt = null;
+			ResultSet rs = null;
+			
+			List<PagesTerms> list = new ArrayList<PagesTerms>();
+			PagesTerms pt = null;
+			Term term = null;
+			
+			try {
+				String query = "SELECT" +
+						" terms.id as termId," +
+						" terms.label as label," +
+						" terms.term_type as term_type," +
+						" pages_terms.id as id," +
+						" pages_terms.weight as weight," +
+						" pages_terms.created_at as created_at," +
+						" pages_terms.updated_at as updated_at," +
+						" pages_terms.source as source" +
+						" FROM pages_terms LEFT JOIN terms ON pages_terms.term_id=terms.id" +
+						" WHERE pages_terms.page_id = ? ";
+				
+				stmt = connection.prepareStatement(query);
+				stmt.setLong(1, pi.getId());
+				
+				rs = stmt.executeQuery();
+				
+				while(rs.next()) {
+					term = new Term();
+					term.setId(rs.getLong("termId"));
+					term.setLabel(rs.getString("label"));
+					term.setTermType(rs.getString("term_type"));
+					
+					pt = new PagesTerms();
+					pt.setPi(pi);
+					pt.setTerm(term);
+					pt.setId(rs.getLong("id"));
+					pt.setWeight(rs.getFloat("weight"));
+					pt.setCreatedAt(new Timestamp(rs.getDate("created_at").getTime()));
+					pt.setUpdatedAt(new Timestamp(rs.getDate("updated_at").getTime()));
+					pt.setSource(rs.getString("source"));
+					
+					list.add(pt);
+				}
+				
+				pi.setPageTermsList(list);
+			} catch(SQLException e) {
+				logger.error("Could not load pageId from cache", e);
+			} finally {
+				SqlUtils.close(rs);
+				SqlUtils.close(stmt);
+			}
+		}
+		
+			public JSONArray extractKeywords(String content, String charset) throws MalformedURLException, IOException {
 				String jsonString = null;
-				String keywords = "";
-	
+				JSONArray jsonArray = null;
+				
 				// initialize connection and set headers
-				HttpURLConnection connection = initConnection(chrset);
+				HttpURLConnection connection = initConnection(charset);
 			    
 		        // establish connection
 			    connection.connect();
@@ -165,6 +244,10 @@ public class CachingPageInformationProviderServiceModule implements ResponseServ
 	
 			    // read response
 			    ByteArrayOutputStream responseOut = readResponseData(connection);
+
+			    if(connection.getResponseCode() == 500) {
+			    	return(null);
+			    }
 	
 			    // close connection
 			    connection.disconnect();
@@ -179,20 +262,15 @@ public class CachingPageInformationProviderServiceModule implements ResponseServ
 			    jsonString = jsonString.trim();
 	
 				try {
-					if(jsonString != null && jsonString.equals("")) {
+					if(!jsonString.equals("")) {
 						JSONParser parser = new JSONParser();
-						JSONArray jsonArray = (JSONArray)parser.parse(jsonString);
-						for (Object jsonObject : jsonArray) {
-							if(((JSONObject)jsonObject).containsKey("name")) {
-								keywords += ((JSONObject)jsonObject).get("name")+",";
-							}
-						}
+						jsonArray = (JSONArray)parser.parse(jsonString);
 					}
 				} catch (ParseException e) {
 					logger.error("JSON parser exception:"+e.getMessage());
 				}
-			    
-			    return keywords;
+
+			    return jsonArray;
 			}
 			
 			private HttpURLConnection initConnection(String charset) throws MalformedURLException, ProtocolException, IOException {
@@ -258,10 +336,52 @@ public class CachingPageInformationProviderServiceModule implements ResponseServ
 			    return(jsonString);
 			}
 			
-		private void savePageInformation(PageInformation pi) {
+			private List<PagesTerms> Json2PagesTerms(JSONArray jsonArray) {
+
+				if(jsonArray == null) {
+					return(null);
+				}
+				
+				Iterator<JSONObject> i = jsonArray.iterator();
+				List<PagesTerms> ptList = new ArrayList<PagesTerms>();
+				PagesTerms pt = null;
+				Term term = null;
+				
+				while(i.hasNext()) {
+					JSONObject jsonObject = (JSONObject)i.next();
+					
+					pt = new PagesTerms();
+					term = new Term();
+					
+					pt.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+					pt.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
+					
+					if(jsonObject.containsKey("name") && !jsonObject.get("name").equals(null)) {
+						term.setLabel((String)jsonObject.get("name"));
+					}
+					if(jsonObject.containsKey("type") && !jsonObject.get("type").equals(null)) {
+						term.setTermType((String)jsonObject.get("type"));
+					}
+					if(jsonObject.containsKey("relevance") && !jsonObject.get("relevance").equals("")) {
+						try {
+							pt.setWeight(new Float(jsonObject.get("relevance").toString()));
+						} catch (NumberFormatException e){
+							logger.debug("Metall meta keywords extraction relevance weight is not a float:"+e.getMessage());
+						}
+					}
+					if(jsonObject.containsKey("source") && !jsonObject.get("source").equals(null)) {
+						pt.setSource((String)jsonObject.get("source"));
+					}
+					pt.setTerm(term);
+					ptList.add(pt);
+				}
+				return(ptList);
+			}
+			
+		private Long savePageInformation(PageInformation pi) {
 			
 			if(connection == null) {
-				return;
+				return null;
 			}
 			
 			String query = "INSERT INTO pages(url, checksum, content_length, keywords) VALUES(?, ?, ?, ?)";
@@ -289,8 +409,132 @@ public class CachingPageInformationProviderServiceModule implements ResponseServ
 				SqlUtils.close(keys);
 				SqlUtils.close(stmt);
 			}
+			return (pi.id);
+		}
+		
+		private void saveTerm(Term term) {
+
+			if(connection == null) {
+				return;
+			}
+			
+			if(term == null) {
+				return;
+			}
+			
+			String query = "INSERT INTO terms(label, term_type) VALUES(?, ?)";
+			
+			PreparedStatement stmt = null;
+			ResultSet keys = null;
+			
+			try {
+				stmt = connection.prepareStatement("SELECT *, count(id) as count FROM terms WHERE label=? AND term_type=?");
+				stmt.setString(1, term.getTermType());
+				stmt.setString(1, term.getLabel());
+				if(term.getTermType() == null) {
+					stmt.setNull(2, Types.VARCHAR);
+				} else {
+					stmt.setString(2, term.getTermType());
+				}
+				stmt.execute();
+				ResultSet rs = stmt.getResultSet();
+				rs.next();
+				if(rs.getLong("count") != 0) {
+					term.setId(rs.getLong("id"));
+					return;
+				}
+				
+				
+				
+				stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+				stmt.setString(1, term.getLabel());
+				if(term.getTermType() == null) {
+					stmt.setNull(2, Types.VARCHAR);
+				} else {
+					stmt.setString(2, term.getTermType());
+				}
+				
+				stmt.execute();
+				
+				keys = stmt.getGeneratedKeys();
+				
+				if(keys.next()) {
+					term.id = keys.getLong(1);
+				}
+			} catch (SQLException e) {
+				logger.error("Could not save terms", e);
+			} finally {
+				SqlUtils.close(keys);
+				SqlUtils.close(stmt);
+			}
+			return;			
 		}
 
+		private void savePagesTerms(PagesTerms pt) {
+
+			if(connection == null) {
+				return;
+			}
+			
+			if(pt == null) {
+				return;
+			}
+			
+			String query = "INSERT INTO pages_terms(page_id, term_id, weight, created_at, updated_at, source) VALUES(?, ?, ?, ?, ?, ?)";
+			
+			PreparedStatement stmt = null;
+			ResultSet keys = null;
+			
+			try {
+				stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+				stmt.setLong(1, pt.getPi().getId());
+				stmt.setLong(2, pt.getTerm().getId());
+				if(pt.getWeight() == null) {
+					stmt.setNull(3, Types.FLOAT);
+				} else {
+					stmt.setFloat(3, pt.getWeight());
+				}
+				stmt.setDate(4, new Date(pt.getCreatedAt().getTime()));
+				stmt.setDate(5, new Date(System.currentTimeMillis()));
+				if(pt.getSource() == null) {
+					stmt.setNull(6, Types.VARCHAR);
+				} else {
+					stmt.setString(6, pt.getSource());
+				}
+				
+				
+				stmt.execute();
+				
+				keys = stmt.getGeneratedKeys();
+				
+				if(keys.next()) {
+					pt.id = keys.getLong(1);
+				}
+			} catch (SQLException e) {
+				logger.error("Could not save terms", e);
+			} finally {
+				SqlUtils.close(keys);
+				SqlUtils.close(stmt);
+			}
+			
+			return;			
+		}
+		
+		private void save(PageInformation pi) {
+			
+			savePageInformation(pi);
+			
+			List<PagesTerms> ptList = pi.getPageTermsList();
+			for (PagesTerms pagesTerms : ptList) {
+				pagesTerms.setPi(pi);
+
+				Term term = pagesTerms.getTerm();
+				saveTerm(term);
+				
+				savePagesTerms(pagesTerms);
+			}
+		}
+		
 		@Override
 		public String getServiceIdentification() {
 			return this.getClass().getName();
