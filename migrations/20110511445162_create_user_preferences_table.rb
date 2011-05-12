9aa35a09df747c738aae09b7921a14510930b03f
Ivan Plesko <ivan.plesko@gmail.com>
2011-05-12 17:31:22 +0200

    Remove ID from migration

--- migrations/20110511445162_create_user_preferences_table.rb
+++ migrations/20110511445162_create_user_preferences_table.rb
@@ -1,6 +1,5 @@
 class CreateUserPreferencesTable < ActiveRecord::Migration
   create_table "user_preferences", :force => true do |t|
-    t.integer  "ID",                :limit => 32
     t.string   "preference_name",   :limit => 50
     t.string   "user",              :limit => 32
     t.string   "preference_value",  :limit => 40
